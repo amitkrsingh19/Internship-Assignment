38d9bb91e1dbcc618d8c1fa28453c2220050609c
Amit Kumar Singh <ranjamit1@gmail.com>
2026-04-07 22:39:17 +0530

    add: cors

--- src/server.js
+++ src/server.js
@@ -18,12 +18,13 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 // Middlewares
-app.use(cors)
+app.use(cors())
 app.use(express.json())
-app.use(express.static(path.join(__dirname,'../public')))
+const publicPath = path.join(__dirname,'../public')
+app.use(express.static(publicPath))
 
 app.get("/",(req,res) => {
-  res.sendFile(path.join(__dirname,"public","index.html"))
+  res.sendFile(path.join(publicPath,"index.html"))
 })
 
 app.use('/api/v1/auth', authRoutes)
